43267986858ac3c4e1598ca3d98c31b54a40a73e
Caleb James DeLisle <cjd@hyperboria.ca>
2013-11-07 16:45:05 +0100

    debugging

--- src/gadget.js
+++ src/gadget.js
@@ -1,26 +1,29 @@
 /*global window, jQuery, rJS */
 (function (window, $, rJS) {
   "use strict";
+  rJS(window).ready(function () {
+console.log(jQuery);
+console.log($);
+console.log(jQuery === $);
+    rJS(window).declareMethod('setContent', function (value) {
+      var data = JSON.parse(value);
+      $(function(){
+          $('#jQuerySheet0').sheet({
+              title: 'Spreadsheet',
+              inlineMenu: undefined,
+              buildSheet: true,
+              urlMenu: false,
+              height: $(window).height() - 20,
+              width: $(window).width() - 20,
+          });
+          $('#jQuerySheet0').getSheet().openSheet($.sheet.makeTable.json(data));
+      });
 
-  rJS(window).declareMethod('setContent', function (value) {
-    var data = JSON.parse(value);
-    $(function(){
-        $('#jQuerySheet0').sheet({
-            title: 'Spreadsheet',
-            inlineMenu: undefined,
-            buildSheet: true,
-            urlMenu: false,
-            height: $(window).height() - 20,
-            width: $(window).width() - 20,
-        });
-        $('#jQuerySheet0').getSheet().openSheet($.sheet.makeTable.json(data));
+    }).declareMethod('getContent', function () {
+      return JSON.stringify($('#jQuerySheet0').getSheet().exportSheet.json());
+
+    }).declareMethod('setReadOnly', function (isReadOnly) {
+
     });
-
-  }).declareMethod('getContent', function () {
-    return JSON.stringify($('#jQuerySheet0').getSheet().exportSheet.json());
-
-  }).declareMethod('setReadOnly', function (isReadOnly) {
-
   });
-
 }(window, jQuery, rJS))
